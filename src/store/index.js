6736a9e6772df734e257fafa80169434b4d49d61
Evan1106 <93518713+Evan1106@users.noreply.github.com>
2022-02-21 10:44:59 +0800

    Mutations Practice

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,23 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-    message: 'hello'
+    name: 'John',
+    age: 55,
+    job: 'teacher'
   },
   getters: {
+    title(state){
+      return `${state.name}: ${state.job}`
+    }
   },
+  //同步執行，使用關鍵字commit
   mutations: {
+    setTitle(state, data){
+      state.name = data.name
+      state.job = data.job
+    }
   },
+  //非同步執行，使用關鍵字dispatch
   actions: {
   },
   modules: {
