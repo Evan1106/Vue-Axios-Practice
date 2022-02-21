a420a24b740ab3b0c41a1ba4673082b0eacc7a58
Evan1106 <93518713+Evan1106@users.noreply.github.com>
2022-02-21 09:54:52 +0800

    Add files via upload

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    message: 'hello'
+  },
+  getters: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
